2d79b28811dca4c8750fe017cc3fb8a23b7bcb2d
N1kkoman <assasinjok@gmail.com>
2023-04-19 17:20:36 +0200

    some edit

--- inc/Device.h
+++ inc/Device.h
@@ -1,6 +1,9 @@
 ﻿#pragma once
 
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
 
 class Device {
 protected:
@@ -74,4 +77,77 @@ public:
 class Owen : public Heating {
 public:
     explicit Owen(const std::string& deviceName) : Heating(deviceName) {}
+};
+
+Device* deviceFabric(const std::string& deviceName) { // создание объекта устройства 
+    Device* currentDevice;
+    switch (static_cast<int>((unsigned char)deviceName[0])) { // проверка первого символа названий приборов
+    case (204):
+        currentDevice = new Mercury(deviceName);
+        break;
+    case (205):
+        currentDevice = new Neva(deviceName);
+        break;
+    case (221):
+        if (236 == static_cast<int>((unsigned char)deviceName[6])) {
+            currentDevice = new Energo(deviceName);
+        }
+        else {
+            currentDevice = new ServiceEnergy(deviceName);
+        }
+        break;
+    case (82):
+        currentDevice = new RealLab(deviceName);
+        break;
+    case (207):
+        currentDevice = new DeviceElectro(deviceName);
+        break;
+    case (79):
+        currentDevice = new Ouman(deviceName);
+        break;
+    case (206):
+        currentDevice = new Owen(deviceName);
+        break;
+    default:
+        currentDevice = nullptr;
+    }
+    return currentDevice;
+}
+
+class SmartHomeSystem {
+private:
+    std::vector<Device*> devices;
+public:
+    SmartHomeSystem() {}
+
+    ~SmartHomeSystem() {
+        for (auto device : devices) {
+            delete device;
+        }
+    }
+
+    void addDevice(Device* device) {
+        devices.push_back(device);
+    }
+
+    void poll() const {
+        for (auto device : devices) {
+            device->poll();
+        }
+    }
+
+    void readConfigFromFile(const std::string& filename) {       
+        std::cout << "Name of Devices: " << "\n\n";
+        std::ifstream config(filename);
+        std::string deviceName;
+        while (std::getline(config, deviceName)) {
+            Device* device = deviceFabric(deviceName);
+            if (device != nullptr) {
+                addDevice(device);
+            }
+            else {
+                std::cout << "Ошибка в названии прибора " << deviceName << std::endl;
+            }
+        }
+    }
 };
--- main.cpp
+++ main.cpp
@@ -3,56 +3,12 @@
 #include <fstream>
 #include <string>
 
-Device* deviceFabric(const std::string& deviceName) { // создание объекта устройства 
-    Device* currentDevice;
-    switch (static_cast<int>((unsigned char)deviceName[0])) { // проверка первого символа названий приборов
-    case (204):
-        currentDevice = new Mercury(deviceName);
-        break;
-    case (205):
-        currentDevice = new Neva(deviceName);
-        break;
-    case (221):
-        if (236 == static_cast<int>((unsigned char)deviceName[6])) {
-            currentDevice = new Energo(deviceName);
-        }
-        else {
-            currentDevice = new ServiceEnergy(deviceName);
-        }
-        break;
-    case (static_cast<int>('R')):
-        currentDevice = new RealLab(deviceName);
-        break;
-    case (207):
-        currentDevice = new DeviceElectro(deviceName);
-        break;
-    case (static_cast<int>('O')):
-        currentDevice = new Ouman(deviceName);
-        break;
-    case (206):
-        currentDevice = new Owen(deviceName);
-        break;
-    default:
-        currentDevice = nullptr;
-    }
-    return currentDevice;
-}
-
 int main() {
     setlocale(LC_ALL, "Russian");
-    std::ifstream devicesList;
-    devicesList.open("devices.txt");
-    std::string currentString;
-    while (getline(devicesList, currentString)) {
-        Device* currentDevice = deviceFabric(currentString);
-        if (currentDevice != nullptr) {
-            currentDevice->poll();
-        }
-        else {
-            std::cout << "Ошибка в названии прибора " << currentString << std::endl;
-        }
-    }
 
-    devicesList.close();
+    SmartHomeSystem system;
+
+    system.readConfigFromFile("devices.txt");
+    system.poll();
     return 0;
 }
